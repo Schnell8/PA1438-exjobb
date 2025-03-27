94857bd11157e0d95e5bbaec22c117e9523125f1
schnell <christopher.schnell@hotmail.com>
2025-03-27 15:19:16 +0100

    register form added

---
+++ frontend/src/components/RegisterForm.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+const RegisterForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleRegister = () => {
+        console.log('Register with: ', username, password, email);
+        // Lägg till kod för att kontrollera username/email + hasha lösenord
+    };
+
+    return (
+        <form onSubmit={handleRegister}>
+            <input
+                type="username" 
+                placeholder="Username" 
+                value={username} 
+                onChange={(e) => setUsername(e.target.value)} 
+                required 
+            />
+            <input
+                type="password" 
+                placeholder="Password" 
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)} 
+                required 
+            />
+            <input
+                type="email" 
+                placeholder="Email" 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)} 
+                required 
+            />
+            <button type="submit">Register</button>
+        </form>
+    );
+};
+
+export default RegisterForm;
