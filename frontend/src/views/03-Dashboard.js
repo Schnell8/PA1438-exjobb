e6618c323e249701d560f4f668c3662f48cf21e2
schnell <christopher.schnell@hotmail.com>
2025-03-27 15:20:16 +0100

    view for dashboard page added

---
+++ frontend/src/views/03-Dashboard.js
@@ -0,0 +1,11 @@
+const DashboardView = () => {
+    return (
+        <div className='dashboardView'>
+            <div className='container'>
+                <h2>Dashboard</h2>
+            </div>
+        </div>
+    );
+};
+
+export default DashboardView;
