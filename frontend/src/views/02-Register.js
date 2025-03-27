6577c394cca9e28e25a3fa4aeb251f0393d446a9
schnell <christopher.schnell@hotmail.com>
2025-03-27 15:19:59 +0100

    view for register page added

---
+++ frontend/src/views/02-Register.js
@@ -0,0 +1,14 @@
+import RegisterForm from '../components/RegisterForm';
+
+const RegisterView = () => {
+    return (
+        <div className='registerView'>
+            <div className='container'>
+                <h2>Register</h2>
+                <RegisterForm />
+            </div>
+        </div>
+    );
+};
+
+export default RegisterView;
