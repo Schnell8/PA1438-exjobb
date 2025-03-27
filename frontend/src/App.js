67c0ce5e3e7784e3569b9ff3fe27e5c887d2fda9
schnell <christopher.schnell@hotmail.com>
2025-03-27 15:20:49 +0100

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import StartView from './views/01-Start';
+import RegisterView from './views/02-Register';
+import DashboardView from './views/03-Dashboard';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<StartView />} />
+          <Route path='/register' element={<RegisterView />} />
+          <Route path='/dashboard' element={<DashboardView />} />
+        </Routes>
+      </Router>
   );
-}
+};
 
 export default App;
