e62125e64c702bc34759c7baeb94eb0c010a7195
schnell <christopher.schnell@hotmail.com>
2025-04-03 14:04:22 +0200

    service for login and registration

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,40 @@
+export const registerUser = async (email, password) => {
+    try {
+        const response = await fetch('http://localhost:5001/auth/register', { // Make dynamical
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password })
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error(data.message || 'Registration failed');
+        }
+
+        return data;
+    } catch (err) {
+        throw new Error(err.message);
+    }
+};
+
+export const loginUser = async (email, password) => {
+    try {
+        const response = await fetch('http://localhost:5001/auth/login', { // Make dynamical
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password })
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error(data.message || 'Login failed');
+        }
+
+        localStorage.setItem('token', data.token);
+        return data;
+    } catch (err) {
+        throw new Error(err.message);
+    }
+};
