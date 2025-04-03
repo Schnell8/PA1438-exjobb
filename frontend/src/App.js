3c303afe4a61b70a159a3c9c468c629be02abb85
schnell <christopher.schnell@hotmail.com>
2025-04-03 14:03:01 +0200

    added protection for views

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import ProtectedView from "./components/ProtectedView";
 import StartView from './views/01-Start';
 import RegisterView from './views/02-Register';
 import DashboardView from './views/03-Dashboard';
-
 import './App.css';
 
 const App = () => {
-  return (
-      <Router>
-        <Routes>
-          <Route path='/' element={<StartView />} />
-          <Route path='/register' element={<RegisterView />} />
-          <Route path='/dashboard' element={<DashboardView />} />
-        </Routes>
-      </Router>
-  );
+    const isAuthenticated = !!localStorage.getItem("token"); // Check if token exists
+
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' element={<StartView />} />
+                <Route path='/register' element={<RegisterView />} />
+                <Route element={<ProtectedView isAuthenticated={isAuthenticated} />}>
+                    <Route path='/dashboard' element={<DashboardView />} />
+                </Route>
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
